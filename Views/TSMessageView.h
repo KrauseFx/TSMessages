e30c1fc46ab85e104b428283185faac464e8af8a
Garrett Richards <grichards@trulia.com>
2013-06-05 11:59:46 -0700

    added TSMessageViewProtocol so navigationbarBottomOfViewController can be used, removed static impl of this method

--- Views/TSMessageView.h
+++ Views/TSMessageView.h
@@ -11,6 +11,12 @@
 
 #define TSMessageViewAlpha 0.95
 
+@protocol TSMessageViewProtocol<NSObject>
+@optional
+/** Can be implemented differently. Is used to define the top position from which the notification flies in from */
+- (CGFloat)navigationbarBottomOfViewController:(UIViewController *)viewController;
+@end
+
 @interface TSMessageView : UIView
 
 + (NSMutableDictionary *)notificationDesign;
@@ -35,6 +41,8 @@
 
 /** Is the message currenlty fully displayed? Is set as soon as the message is really fully visible */
 @property (nonatomic, assign) BOOL messageIsFullyDisplayed;
+
+@property(nonatomic, assign) id <TSMessageViewProtocol> delegate;
 
 /** Inits the notification view. Do not call this from outside this library.
  @param title The title of the notification view
