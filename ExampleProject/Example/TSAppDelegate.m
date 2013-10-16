0186473ed2c906314f221bfca4c324c247215f9c
Hannes Oud <hannes.oud.dev@gmail.com>
2013-10-16 10:59:45 +0200

    AppDelegate window property: Resolved warning "Auto property synthesis is synthesizing property not explicitly synthesized"

--- ExampleProject/Example/TSAppDelegate.m
+++ ExampleProject/Example/TSAppDelegate.m
@@ -10,6 +10,8 @@
 
 @implementation TSAppDelegate
 
+@synthesize window;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
