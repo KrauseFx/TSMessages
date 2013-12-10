efadcbe5e3a55172c510c21fa40509df3a4c2ac2
Tobias Conradi <github@tobias-conradi.de>
2013-12-10 11:58:29 +0100

    fix dismiss animation when still animating

--- TSMessages/Classes/TSMessageDynamicAnimation.m
+++ TSMessages/Classes/TSMessageDynamicAnimation.m
@@ -9,31 +9,36 @@
 #import "TSMessageDynamicAnimation.h"
 #import "TSMessageView.h"
 
+#define iOS7Enabled __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
+
 @interface TSMessageDynamicAnimation () <UIDynamicAnimatorDelegate>
+#if iOS7Enabled
 @property (nonatomic, strong) UIDynamicAnimator *animator;
+#endif
 @property (nonatomic, copy) void (^completionBlock)(void);
 
 @end
 
 @implementation TSMessageDynamicAnimation
 
-#define iOS7Enabled __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
+static TSMessageDynamicAnimation *TSMessageDynamicAnimationInstance = nil;
 
 + (void)animateMessageView:(TSMessageView *)view toFrame:(CGRect)targetFrame appearing:(BOOL)isAppearing completion:(void (^)(void))completionBlock {
 #if iOS7Enabled
+    // currently only one animation per time is supported
+    if (TSMessageDynamicAnimationInstance) {
+        [TSMessageDynamicAnimationInstance stopAnimation];
+        TSMessageDynamicAnimationInstance = nil;
+    }
     if (isAppearing && [UIDynamicAnimator class]) {
         TSMessageDynamicAnimation *animationObject = [TSMessageDynamicAnimation new];
         animationObject.completionBlock = ^{
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-retain-cycles"
-            [animationObject class]; // hold onto self until the animator completes
-#pragma clang diagnostic pop
-            
             if (completionBlock) {
                 completionBlock();
             }
         };
         [animationObject animateView:view toFrame:targetFrame];
+        TSMessageDynamicAnimationInstance = animationObject;
     } else {
         [super animateMessageView:view toFrame:targetFrame appearing:isAppearing completion:completionBlock];
     }
@@ -45,6 +50,10 @@
     
 }
 
+- (void)stopAnimation {
+    [self.animator removeAllBehaviors];
+    self.animator = nil;
+}
 #if iOS7Enabled
 
 #pragma mark - Dynamics
