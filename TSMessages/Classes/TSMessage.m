KrauseFx/TSMessages

commit bf57c78fd0f0463d0630fbc1a323179b228b5591

Added a callback block to TSMessage dismissActiveNotificationWithCompletion after the animation is completed.

TSMessages/Classes/TSMessage.m
@@ -21,7 +21,7 @@
 @property (nonatomic, strong) NSMutableArray *messages;
 
 - (void)fadeInCurrentNotification;
-- (void)fadeOutNotification:(TSMessageView *)currentView;
+- (void)fadeOutNotification:(TSMessageView *)currentView animationFinishedBlock:(void (^)())animationFinished;
 
 @end
 
@@ -314,7 +314,7 @@ __weak static UIViewController *_defaultViewController;
     }
 }
 
-- (void)fadeOutNotification:(TSMessageView *)currentView
+- (void)fadeOutNotification:(TSMessageView *)currentView animationFinishedBlock:(void (^)())animationFinished
 {
     currentView.messageIsFullyDisplayed = NO;
     [NSObject cancelPreviousPerformRequestsWithTarget:self
@@ -353,10 +353,19 @@ __weak static UIViewController *_defaultViewController;
          {
              [self fadeInCurrentNotification];
          }
+         
+         if(animationFinished) {
+             animationFinished();
+         }
      }];
 }
 
 + (BOOL)dismissActiveNotification
+{
+    return [self dismissActiveNotificationWithCompletion:nil];
+}
+
++ (BOOL)dismissActiveNotificationWithCompletion:(void (^)())completion
 {
     if ([[TSMessage sharedMessage].messages count] == 0) return NO;
     
@@ -366,7 +375,11 @@ __weak static UIViewController *_defaultViewController;
                        TSMessageView *currentMessage = [[TSMessage sharedMessage].messages objectAtIndex:0];
                        if (currentMessage.messageIsFullyDisplayed)
                        {
-                           [[TSMessage sharedMessage] fadeOutNotification:currentMessage];
+                           [[TSMessage sharedMessage] fadeOutNotification:currentMessage animationFinishedBlock:^{
+                               if(completion) {
+                                   completion();
+                               }
+                           }];
                        }
                    });
     return YES;

TSMessages/Classes/TSMessage.h
@@ -147,6 +147,13 @@ typedef NS_ENUM(NSInteger,TSMessageNotificationDuration) {
  */
 + (BOOL)dismissActiveNotification;
 
+/** Fades out the currently displayed notification with a completion block after the animation has finished. If another notification is in the queue,
+ the next one will be displayed automatically
+ @return YES if the currently displayed notification was successfully dismissed. NO if no notification
+ was currently displayed.
+ */
++ (BOOL)dismissActiveNotificationWithCompletion:(void (^)())completion;
+
 /** Use this method to set a default view controller to display the messages in */
 + (void)setDefaultViewController:(UIViewController *)defaultViewController;
 

commit 6b049815b4eb44e1a08387129d04d640757524aa

Fix crash caused by bf57c78fd0f0463d0630fbc1a323179b228b5591

ExampleProject/Example.xcworkspace/xcshareddata/Example.xccheckout
@@ -5,34 +5,34 @@
 	<key>IDESourceControlProjectFavoriteDictionaryKey</key>
 	<false/>
 	<key>IDESourceControlProjectIdentifier</key>
-	<string>78E13C39-DCE2-42E8-B56F-F65483531CDC</string>
+	<string>F6FF7C22-1562-4A01-9CD1-A3C34B7E21C3</string>
 	<key>IDESourceControlProjectName</key>
 	<string>Example</string>
 	<key>IDESourceControlProjectOriginsDictionary</key>
 	<dict>
-		<key>A82D8531-0480-4292-B9CA-F43F680C1C78</key>
-		<string>https://github.com/mRs-/TSMessages.git</string>
+		<key>56C72571-8325-46B6-BC46-920E7DE5B629</key>
+		<string>https://github.com/toursprung/TSMessages.git</string>
 	</dict>
 	<key>IDESourceControlProjectPath</key>
 	<string>ExampleProject/Example.xcworkspace</string>
 	<key>IDESourceControlProjectRelativeInstallPathDictionary</key>
 	<dict>
-		<key>A82D8531-0480-4292-B9CA-F43F680C1C78</key>
+		<key>56C72571-8325-46B6-BC46-920E7DE5B629</key>
 		<string>../..</string>
 	</dict>
 	<key>IDESourceControlProjectURL</key>
-	<string>https://github.com/mRs-/TSMessages.git</string>
+	<string>https://github.com/toursprung/TSMessages.git</string>
 	<key>IDESourceControlProjectVersion</key>
 	<integer>110</integer>
 	<key>IDESourceControlProjectWCCIdentifier</key>
-	<string>A82D8531-0480-4292-B9CA-F43F680C1C78</string>
+	<string>56C72571-8325-46B6-BC46-920E7DE5B629</string>
 	<key>IDESourceControlProjectWCConfigurations</key>
 	<array>
 		<dict>
 			<key>IDESourceControlRepositoryExtensionIdentifierKey</key>
 			<string>public.vcs.git</string>
 			<key>IDESourceControlWCCIdentifierKey</key>
-			<string>A82D8531-0480-4292-B9CA-F43F680C1C78</string>
+			<string>56C72571-8325-46B6-BC46-920E7DE5B629</string>
 			<key>IDESourceControlWCCName</key>
 			<string>TSMessages</string>
 		</dict>

TSMessages/Classes/TSMessage.m
@@ -20,9 +20,6 @@
 /** The queued messages (TSMessageView objects) */
 @property (nonatomic, strong) NSMutableArray *messages;
 
-- (void)fadeInCurrentNotification;
-- (void)fadeOutNotification:(TSMessageView *)currentView animationFinishedBlock:(void (^)())animationFinished;
-
 @end
 
 @implementation TSMessage
@@ -312,6 +309,11 @@ __weak static UIViewController *_defaultViewController;
     } else {
         return NO;
     }
+}
+
+- (void)fadeOutNotification:(TSMessageView *)currentView
+{
+    [self fadeOutNotification:currentView animationFinishedBlock:nil];
 }
 
 - (void)fadeOutNotification:(TSMessageView *)currentView animationFinishedBlock:(void (^)())animationFinished