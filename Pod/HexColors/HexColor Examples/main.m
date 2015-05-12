a8bbd565b6fe8052e1f415cb7025e2cda5a4f66f
Ben Rigas <ben.rigas@modernizingmedicine.com>
2015-05-12 10:07:18 -0400

    update hex colors

---
+++ Pod/HexColors/HexColor Examples/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HexColor Examples
+//
+//  Created by Marius Landwehr on 21.05.13.
+//  Copyright (c) 2013 Marius Landwehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
